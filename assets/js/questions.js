7461cbb28193978252a26dec62eb48266412c5fe
Thanh Ngo <thanhngo26@hotmail.com>
2021-01-26 19:21:51 -0800

    logic and questions created

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["script", "javascripts", "js", "scripting"],
+      answer: "script"
+    },
+    {
+      title: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choices: ["script href='xxx.js'", "script name='xxx.js'", "script src='xxx.js'", "script file='xxx.js"],
+      answer: "script src='xxx.js'"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+  ];
